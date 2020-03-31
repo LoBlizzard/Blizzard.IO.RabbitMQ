5ebd34301eabc5d2448b1bb46a9dff3b03d3df64
IdoRozval <idorozval@gmail.com>
2020-03-31 22:13:45 +0300

    off to fix core

--- Blizzard.IO.RabbitMQ/Implementaions/NetqRabbitConsumer.cs
+++ Blizzard.IO.RabbitMQ/Implementaions/NetqRabbitConsumer.cs
@@ -1,22 +1,51 @@
 ﻿using System;
 using Blizzard.IO.Core;
 using Blizzard.IO.RabbitMQ.Entities;
+using EasyNetQ;
+using EasyNetQ.Topology;
 
 namespace Blizzard.IO.RabbitMQ.Implementaions
 {
-    public class RabbitConsumer<TData> : IConsumer<TData>, IConsumerWithMetadata<TData, RabbitMessageProperties>
+    public class NetqRabbitConsumer<TData> : IConsumer<TData>, IConsumerWithMetadata<TData, RabbitMessageProperties>
     {
+        private readonly IBus _netqBus;
+        private readonly IDeserializer<TData> _deserializer;
+        private readonly IConcreteTypeDeserializer<TData> _concreteTypeDeserializer;
+        private readonly IQueue _sourceQueue;
+
         public event Action<TData> MessageReceived;
         public event Action<TData, RabbitMessageProperties> MessageWithMetadataReceived;
 
+        public NetqRabbitConsumer(IBus netqBus, IQueue sourceQueue,
+            IDeserializer<TData> deserializer)
+        {
+            _netqBus = netqBus ?? throw new ArgumentNullException(nameof(netqBus));
+            _sourceQueue = sourceQueue ?? throw new ArgumentNullException(nameof(sourceQueue));
+            _deserializer = deserializer ?? throw new ArgumentNullException(nameof(deserializer));
+        }
+
+        public NetqRabbitConsumer(IBus netqBus, IQueue sourceQueue,
+            IConcreteTypeDeserializer<TData> concreteTypeDeserializer)
+        {
+            _netqBus = netqBus ?? throw new ArgumentNullException(nameof(netqBus));
+            _sourceQueue = sourceQueue ?? throw new ArgumentNullException(nameof(sourceQueue));
+            _concreteTypeDeserializer = concreteTypeDeserializer ?? throw new ArgumentNullException(nameof(concreteTypeDeserializer));
+        }
+
         public void Start()
         {
-            throw new NotImplementedException();
+            _netqBus.Advanced.Consume(_sourceQueue, (messageBytes, messageProperties, messageInfo) =>
+            {
+                if (_concreteTypeDeserializer != null)
+                {
+                    //TData data = _concreteTypeDeserializer.Deserialize()
+                }
+            })
         }
 
         public void Stop()
         {
-            throw new NotImplementedException();
+            _netqBus.Dispose();
         }
     }
 }
