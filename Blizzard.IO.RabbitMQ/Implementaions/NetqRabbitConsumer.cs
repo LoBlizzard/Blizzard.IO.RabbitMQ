27282b0cd6416eecca5e54d80eeb86c8deb8bdfd
IdoRozval <idorozval@gmail.com>
2020-03-31 22:10:16 +0300

    off to fix core

---
+++ Blizzard.IO.RabbitMQ/Implementaions/NetqRabbitConsumer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Blizzard.IO.Core;
+using Blizzard.IO.RabbitMQ.Entities;
+
+namespace Blizzard.IO.RabbitMQ.Implementaions
+{
+    public class RabbitConsumer<TData> : IConsumer<TData>, IConsumerWithMetadata<TData, RabbitMessageProperties>
+    {
+        public event Action<TData> MessageReceived;
+        public event Action<TData, RabbitMessageProperties> MessageWithMetadataReceived;
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
