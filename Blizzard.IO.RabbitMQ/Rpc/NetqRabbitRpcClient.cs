606af65b4b234fb13948e90d994bc1d927268d40
alloncm <alloncm@gmail.com>
2020-04-01 21:09:49 +0300

    added the rpc client

---
+++ Blizzard.IO.RabbitMQ/Rpc/NetqRabbitRpcClient.cs
@@ -0,0 +1,32 @@
+﻿using Blizzard.IO.Core.Rpc;
+using System;
+using System.Threading.Tasks;
+
+namespace Blizzard.IO.RabbitMQ.Rpc
+{
+    public class NetqRabbitRpcClient : IRpcClient
+    {
+        private readonly INetqRpcRabbitConnection<Func<Type, object>> _netqRpcRabbitConnection;
+
+        public NetqRabbitRpcClient(INetqRpcRabbitConnection<Func<Type, object>> netqRpcRabbitConnection)
+        {
+            _netqRpcRabbitConnection = netqRpcRabbitConnection;
+        }
+
+        public TRespond Request<TRequest, TRespond>(TRequest request)
+        where TRequest : class
+        where TRespond : class
+        {
+            var func = _netqRpcRabbitConnection.RabbitBus.Request<TRequest, Func<Type, object>>(request);
+            return (TRespond)func(typeof(TRespond));
+        }
+
+        public async Task<TRespond> RequestASync<TRequest, TRespond>(TRequest request)
+        where TRequest : class
+        where TRespond : class
+        {
+            Func<Type, object> func = await _netqRpcRabbitConnection.RabbitBus.RequestAsync<TRequest, Func<Type, object>>(request);
+            return (TRespond)func(typeof(TRespond));
+        }
+    }
+}
