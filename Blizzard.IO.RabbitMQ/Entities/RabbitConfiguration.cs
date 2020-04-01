5f1ade17c03ce10de708abd4352d99b6a19ef18d
IdoRozval <idorozval@gmail.com>
2020-04-01 17:18:05 +0300

    Cleared redundent full objects from binding lists

--- Blizzard.IO.RabbitMQ/Entities/RabbitConfiguration.cs
+++ Blizzard.IO.RabbitMQ/Entities/RabbitConfiguration.cs
@@ -6,7 +6,7 @@ namespace Blizzard.IO.RabbitMQ.Entities
     {
         public List<RabbitExchange> Exchanges { get; set; }
         public List<RabbitQueue> Queues { get; set; }
-        public List<(RabbitExchange, RabbitQueue, string)> ExchangeToQueueBindings { get; set; }
-        public List<(RabbitExchange, RabbitExchange, string)> ExchangeToExchangeBindings { get; set; }
+        public List<(string, string, string)> ExchangeToQueueBindings { get; set; }
+        public List<(string, string, string)> ExchangeToExchangeBindings { get; set; }
     }
 }
