9ea3c8a0449e36d50bcead9ebc3f8b95cf6d3ab7
IdoRozval <idorozval@gmail.com>
2020-04-01 17:26:56 +0300

    Added binding object for readability purposes

--- Blizzard.IO.RabbitMQ/Entities/RabbitConfiguration.cs
+++ Blizzard.IO.RabbitMQ/Entities/RabbitConfiguration.cs
@@ -6,7 +6,7 @@ namespace Blizzard.IO.RabbitMQ.Entities
     {
         public List<RabbitExchange> Exchanges { get; set; }
         public List<RabbitQueue> Queues { get; set; }
-        public List<(string, string, string)> ExchangeToQueueBindings { get; set; }
-        public List<(string, string, string)> ExchangeToExchangeBindings { get; set; }
+        public List<RabbitBinding> ExchangeToQueueBindings { get; set; }
+        public List<RabbitBinding> ExchangeToExchangeBindings { get; set; }
     }
 }
