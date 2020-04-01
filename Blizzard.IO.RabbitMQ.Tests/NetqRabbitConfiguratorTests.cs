3852afbece0939cf52ca1450530af6ee86f13f13
IdoRozval <idorozval@gmail.com>
2020-04-01 20:38:16 +0300

    Proceeding with tests

---
+++ Blizzard.IO.RabbitMQ.Tests/NetqRabbitConfiguratorTests.cs
@@ -0,0 +1,158 @@
+﻿using Blizzard.IO.RabbitMQ.Entities;
+using EasyNetQ;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Blizzard.IO.RabbitMQ.Tests
+{
+    [TestFixture]
+    public class NetqRabbitConfiguratorTests
+    {
+        private RabbitConfiguration _configuration;
+        private NetqRabbitConfigurator _netqRabbitConfigurator;
+        private Mock<IBus> _netqBusMock;
+        private Mock<IAdvancedBus> _netqAdvancedBusMock;
+        private Mock<ILogger<NetqRabbitConfigurator>> _loggerMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _configuration = new RabbitConfiguration
+            {
+                Exchanges = new List<RabbitExchange>(),
+                Queues = new List<RabbitQueue>(),
+                ExchangeToExchangeBindings = new List<RabbitBinding>(),
+                ExchangeToQueueBindings = new List<RabbitBinding>()
+            };
+
+            _loggerMock = new Mock<ILogger<NetqRabbitConfigurator>>();
+            _netqAdvancedBusMock = new Mock<IAdvancedBus>();
+            _netqBusMock = new Mock<IBus>();
+            _netqBusMock.SetupGet(bus => bus.Advanced).Returns(_netqAdvancedBusMock.Object);
+            _netqRabbitConfigurator = new NetqRabbitConfigurator(_netqBusMock.Object, _loggerMock.Object);
+        }
+
+        [Test]
+        public void Configure_OnRabbitConfigurationNull_ShouldThrowNullArgumentException()
+        {
+            // Act + Assert
+            Assert.That(() => _netqRabbitConfigurator.Configure(null), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void Configure_OnRabbitExchangeFoundConfigurationExchanges_ShouldDeclareThatExchangePropertiesOnBus()
+        {
+            // Arrange
+            var exchange = new RabbitExchange
+            {
+                Name = "0",
+                Type = RabbitExchangeType.Direct,
+                Passive = false,
+                Durable = false,
+                AutoDelete = false,
+                Internal = false,
+                AlternateExchange = "2",
+                Delayed = false
+            };
+            _configuration.Exchanges.Add(exchange);
+
+            // Act
+            _netqRabbitConfigurator.Configure(_configuration);
+
+            // Assert
+            _netqAdvancedBusMock.Verify(bus => bus.ExchangeDeclare(
+                exchange.Name,
+                Helpers.ExchangeTypeToStringResolver[exchange.Type],
+                exchange.Passive,
+                exchange.Durable,
+                exchange.AutoDelete,
+                exchange.Internal,
+                exchange.AlternateExchange,
+                exchange.Delayed), Times.Once);
+        }
+
+        [Test]
+        public void Configure_OnRabbitQueue_ShouldDeclareThatExchangesPropertiesOnBus()
+        {
+            // Arrange
+            var queue = new RabbitQueue
+            {
+                Name = "0",
+                Passive = false,
+                Durable = false,
+                Exclusive = false,
+                AutoDelete = false,
+                PerQueueMessageTtl = 12,
+                Expires = 3,
+                MaxPriority = 4,
+                DeadLetterExchange = "are",
+                DeadLetterRoutingKey = "really...",
+                MaxLength = 10,
+                MaxLengthBytes = 1
+            };
+            _configuration.Queues.Add(queue);
+
+            // Act
+            _netqRabbitConfigurator.Configure(_configuration);
+
+            // Assert
+            _netqAdvancedBusMock.Verify(bus => bus.QueueDeclare(
+                queue.Name,
+                queue.Passive,
+                queue.Durable,
+                queue.Exclusive,
+                queue.AutoDelete,
+                queue.PerQueueMessageTtl,
+                queue.Expires,
+                queue.MaxPriority,
+                queue.DeadLetterExchange,
+                queue.DeadLetterRoutingKey,
+                queue.MaxLength,
+                queue.MaxLengthBytes), Times.Once);
+        }
+
+        [Test]
+        public void Configure_OnExchangeToExchangeBindingFound_ShouldBindThatRelation()
+        {
+            // Arrange
+            var binding = new RabbitBinding
+            {
+                SourceName = "source",
+                DestName = "dest",
+                RoutingKey = "/"
+            };
+            _configuration.ExchangeToExchangeBindings.Add(binding);
+
+            // Act
+            _netqRabbitConfigurator.Configure(_configuration);
+
+            // Assert
+            _netqAdvancedBusMock.Verify(bus => 
+                bus.Bind(binding.SourceName, binding.DestName, binding.RoutingKey), Times.Once);
+        }
+
+        [Test]
+        public void Configure_OnExchangeToQueueBindingFound_ShouldBindThatRelation()
+        {
+            // Arrange
+            var binding = new RabbitBinding
+            {
+                SourceName = "source",
+                DestName = "dest",
+                RoutingKey = "/"
+            };
+            _configuration.ExchangeToQueueBindings.Add(binding);
+
+            // Act
+            _netqRabbitConfigurator.Configure(_configuration);
+
+            // Assert
+            _netqAdvancedBusMock.Verify(bus =>
+                bus.Bind(binding.SourceName, binding.DestName, binding.RoutingKey), Times.Once);
+        }
+    }
+}
+    
--- Blizzard.IO.RabbitMQ/NetqRabbitConfigurator.cs
+++ Blizzard.IO.RabbitMQ/NetqRabbitConfigurator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Blizzard.IO.RabbitMQ.Entities;
 using EasyNetQ;
 using EasyNetQ.Topology;
+using Microsoft.Extensions.Logging;
 
 namespace Blizzard.IO.RabbitMQ
 {
@@ -11,10 +12,12 @@ namespace Blizzard.IO.RabbitMQ
         private readonly IBus _netqBus;
         private readonly IDictionary<string, IExchange> _nameToNetqExchange;
         private readonly IDictionary<string, IQueue> _nameToNetqQueue;
+        private readonly ILogger<NetqRabbitConfigurator> _logger;
 
-        public NetqRabbitConfigurator(IBus netqBus)
+        public NetqRabbitConfigurator(IBus netqBus, ILogger<NetqRabbitConfigurator> logger)
         {
             _netqBus = netqBus ?? throw new ArgumentNullException(nameof(netqBus));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _nameToNetqExchange = new Dictionary<string, IExchange>();
             _nameToNetqQueue = new Dictionary<string, IQueue>();
         }
@@ -22,11 +25,14 @@ namespace Blizzard.IO.RabbitMQ
         public void Configure(RabbitConfiguration configuration)
         {
             _nameToNetqExchange.Clear();
-            _nameToNetqQueue.Clear(); 
+            _nameToNetqQueue.Clear();
+            _logger.LogInformation("Cleared all previous declarations");
+
             DeclareExchanges(configuration.Exchanges);
             DeclareQueues(configuration.Queues);
             DeclareExchangeToQueueBindings(configuration.ExchangeToQueueBindings);
             DeclareExchangeToExchangeBindings(configuration.ExchangeToExchangeBindings);
+            _logger.LogInformation("Declared topology succesfully on bus");
         }
 
         private void DeclareExchangeToExchangeBindings(List<RabbitBinding> exchangeToExchangeBindings)
@@ -39,6 +45,8 @@ namespace Blizzard.IO.RabbitMQ
                     binding.RoutingKey
                     );
             }
+
+            _logger.LogDebug("Declared all exchange to exchange bindings");
         }
 
         private void DeclareExchangeToQueueBindings(List<RabbitBinding> exchangeToQueueBinding)
@@ -51,6 +59,8 @@ namespace Blizzard.IO.RabbitMQ
                     binding.RoutingKey
                     );
             }
+
+            _logger.LogDebug("Declared all exchange to queue bindings");
         }
 
         private void DeclareQueues(List<RabbitQueue> queues)
@@ -71,6 +81,8 @@ namespace Blizzard.IO.RabbitMQ
                     queue.MaxLength,
                     queue.MaxLengthBytes
                     );
+
+                _logger.LogDebug($"Declared queue: {queue.Name} succesfully");
             }
         }
 
@@ -88,6 +100,8 @@ namespace Blizzard.IO.RabbitMQ
                     exchange.AlternateExchange,
                     exchange.Delayed
                     );
+
+                _logger.LogDebug($"Declared exchange: {exchange.Name} succesfully");
             }
         }
     }
