8a78791a1f75d5c92411c7162764b551cccc1ad1
IdoRozval <idorozval@gmail.com>
2020-04-01 16:10:46 +0300

    Added converter

--- Blizzard.IO.RabbitMQ/Entities/RabbitMessageProperties.cs
+++ Blizzard.IO.RabbitMQ/Entities/RabbitMessageProperties.cs
@@ -12,11 +12,11 @@ namespace Blizzard.IO.RabbitMQ.Entities
         public string ContentType { get; set; }
         public string ContentEncoding { get; set; }
         public string MessageId { get; set; }
-        public string CorellationId { get; set; }
+        public string CorrelationId { get; set; }
         public string ReplyTo { get; set; }
-        public DateTime Timestamp { get; set; }
+        public long Timestamp { get; set; }
         public string UserId { get; set; }
-        public string AddId { get; set; }
+        public string AppId { get; set; }
         public string ClusterId { get; set; }
         public string Expiration { get; set; }
         public bool Persistent { get; set; }
---
+++ Blizzard.IO.RabbitMQ/RabbitPropertiesConverter.cs
@@ -0,0 +1,55 @@
+﻿using Blizzard.IO.Core;
+using Blizzard.IO.RabbitMQ.Entities;
+using EasyNetQ;
+using System;
+using System.Collections.Generic;
+
+namespace Blizzard.IO.RabbitMQ
+{
+    public class RabbitPropertiesConverter :
+        IConverter<RabbitMessageProperties, MessageProperties>,
+        IConverter<MessageProperties, RabbitMessageProperties>
+    {
+        public MessageProperties Convert(RabbitMessageProperties obj)
+        {
+            return new MessageProperties
+            {
+                DeliveryMode = obj.DeliveryMode,
+                Type = obj.Type,
+                Headers = obj.Headers as Dictionary<string, object>,
+                ContentType = obj.ContentType,
+                ContentEncoding = obj.ContentEncoding,
+                MessageId = obj.MessageId,
+                CorrelationId = obj.CorrelationId,
+                ReplyTo = obj.ReplyTo,
+                Timestamp = obj.Timestamp,
+                UserId = obj.UserId,
+                AppId = obj.AppId,
+                ClusterId = obj.ClusterId,
+                Expiration = obj.Expiration,
+                Priority = obj.Priority
+            };
+        }
+
+        public RabbitMessageProperties Convert(MessageProperties obj)
+        {
+            return new RabbitMessageProperties
+            {
+                DeliveryMode = obj.DeliveryMode,
+                Type = obj.Type,
+                Headers = obj.Headers as Dictionary<string, object>,
+                ContentType = obj.ContentType,
+                ContentEncoding = obj.ContentEncoding,
+                MessageId = obj.MessageId,
+                CorrelationId = obj.CorrelationId,
+                ReplyTo = obj.ReplyTo,
+                Timestamp = obj.Timestamp,
+                UserId = obj.UserId,
+                AppId = obj.AppId,
+                ClusterId = obj.ClusterId,
+                Expiration = obj.Expiration,
+                Priority = obj.Priority
+            };
+        }
+    }
+}
