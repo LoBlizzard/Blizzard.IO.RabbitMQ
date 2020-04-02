b56bbcf43c5b4a800bf0b3f89bd6553fca980979
IdoRozval <idorozval@gmail.com>
2020-04-02 17:58:54 +0300

    Feature deserializer

---
+++ Blizzard.IO.RabbitMQ/JsonAbstractTypeDeserializer.cs
@@ -0,0 +1,15 @@
+﻿using Blizzard.IO.Core;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace Blizzard.IO.RabbitMQ
+{
+    public class JsonAbstractTypeDeserializer<TData> : IConcreteTypeDeserializer<TData>
+    {
+        public TData Deserialize(byte[] bytes, Type type)
+        {
+            return (TData) JsonConvert.DeserializeObject(Encoding.UTF8.GetString(bytes), type);
+        }
+    }
+}
---
+++ Blizzard.IO.RabbitMQ/JsonDeserializer.cs
@@ -0,0 +1,14 @@
+﻿using Blizzard.IO.Core;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Blizzard.IO.RabbitMQ
+{
+    public class JsonDeserializer<TData> : IDeserializer<TData>
+    {
+        public TData Deserialize(byte[] bytes)
+        {
+            return JsonConvert.DeserializeObject<TData>(Encoding.UTF8.GetString(bytes));
+        }
+    }
+}
