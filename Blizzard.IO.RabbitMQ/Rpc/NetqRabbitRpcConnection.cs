02a5f7283c769059550b597bbc7c40d9d730ae96
alloncm <alloncm@gmail.com>
2020-04-05 20:15:45 +0300

    fixed the connection

--- Blizzard.IO.RabbitMQ/Rpc/NetqRabbitRpcConnection.cs
+++ Blizzard.IO.RabbitMQ/Rpc/NetqRabbitRpcConnection.cs
@@ -4,6 +4,7 @@ using EasyNetQ.Producer;
 using System;
 using ISerializer = Blizzard.IO.Core.Rpc.ISerializer;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace Blizzard.IO.RabbitMQ.Rpc
 {
@@ -67,6 +68,30 @@ namespace Blizzard.IO.RabbitMQ.Rpc
         public void Dispose()
         {
             Bus.Dispose();
+        }
+
+        public Func<Type, object> Request<TRequest>(TRequest request)
+            where TRequest:class
+        {
+            return Bus.Request<TRequest,Func<Type,object>>(request);
+        }
+
+        public Task<Func<Type, object>> RequestAsync<TRequest>(TRequest request) 
+            where TRequest : class
+        {
+            return Bus.RequestAsync<TRequest, Func<Type, object>>(request);
+        }
+
+        public IDisposable Respond<TRespond>(Func<Func<Type, object>, TRespond> callback) 
+            where TRespond : class
+        {
+            return Bus.Respond(callback);
+        }
+
+        public IDisposable RespondAsync<TRespond>(Func<Func<Type, object>, Task<TRespond>> callback) 
+            where TRespond : class
+        {
+            return Bus.RespondAsync(callback);
         }
     }
 }
