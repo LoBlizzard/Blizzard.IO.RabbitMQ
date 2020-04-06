e9c7a1c9f3dd680cbe2519bde3150296f19fdddc
alloncm <alloncm@gmail.com>
2020-04-06 19:31:27 +0300

    more fixes

--- Blizzard.IO.RabbitMQ.Builders/Rpc/BaseNetqRabbitRpcBuilder.cs
+++ Blizzard.IO.RabbitMQ.Builders/Rpc/BaseNetqRabbitRpcBuilder.cs
@@ -4,6 +4,7 @@ using Blizzard.IO.RabbitMQ.Rpc;
 using System;
 using Blizzard.IO.RabbitMQ.Entities.Rpc;
 using Microsoft.Extensions.Logging;
+using Blizzard.IO.Serialization.Rpc;
 
 namespace Blizzard.IO.RabbitMQ.Builders.Rpc
 {
@@ -11,6 +12,24 @@ namespace Blizzard.IO.RabbitMQ.Builders.Rpc
     {
         private static Dictionary<RpcConnectionKey, INetqRabbitRpcConnection<Func<Type, object>>> netqRpcConnections = new Dictionary<RpcConnectionKey, INetqRabbitRpcConnection<Func<Type, object>>>();
 
+        protected ISerializer Serializer = new JsonSerializer();
+        protected string Hostname = "localhost";
+        protected string Password = "guest";
+        protected string Username = "guest";
+        protected RpcConfiguration RpcConfiguration = new RpcConfiguration
+        {
+            ExchangeNameProvider = type => "RPC_EXCHANGE",
+            RoutingKeyProvider = type => type.ToString()
+        };
+        protected RpcMessageType RpcMessageType = RpcMessageType.Concrete;
+        protected ushort Timeout = 10;
+        protected string Product = null;
+        protected string Platform = null;
+        protected string VirtualHost = null;
+        protected int RequestHeartbeat = 10;
+        protected int PrefetchCount = 50;
+        protected bool PublisherConfirms = false;
+        protected bool PersistentMessages = true;
         protected readonly ILoggerFactory LoggerFactory;
 
         public BaseNetqRabbitRpcBuilder(ILoggerFactory loggerFactory)
@@ -18,30 +37,16 @@ namespace Blizzard.IO.RabbitMQ.Builders.Rpc
             LoggerFactory = loggerFactory;
         }
 
-        protected INetqRabbitRpcConnection<Func<Type, object>> InitConnection(
-            string host,
-            string password,
-            string username,
-            RpcConfiguration configuration,
-            ISerializer serializer,
-            RpcMessageType rpcMessageType = RpcMessageType.Concrete,
-            ushort timeout = 10,
-            string product = null,
-            string platform = null,
-            string virtualHost = null,
-            int requestHeartbeat = 10,
-            int prefetchCount = 50,
-            bool publisherConfirms = false,
-            bool persistentMessages = true)
+        protected INetqRabbitRpcConnection<Func<Type, object>> InitConnection()
         {
-            RpcConnectionKey busKey = new RpcConnectionKey(host, username, password, rpcMessageType, serializer.GetType());
+            RpcConnectionKey busKey = new RpcConnectionKey(Hostname, Username, Password, RpcMessageType, Serializer.GetType());
             if (netqRpcConnections.ContainsKey(busKey))
             {
                 return netqRpcConnections[busKey];
             }
 
-            INetqRabbitRpcConnection<Func<Type,object>> connection = new NetqRabbitRpcConnection(configuration, host, username, password,LoggerFactory, serializer, requestHeartbeat, (ushort)prefetchCount, timeout,
-                publisherConfirms, persistentMessages, product, platform, virtualHost);
+            INetqRabbitRpcConnection<Func<Type,object>> connection = new NetqRabbitRpcConnection(RpcConfiguration, Hostname, Username, Password,LoggerFactory,
+               Serializer, RequestHeartbeat, (ushort)PrefetchCount,Timeout,PublisherConfirms, PersistentMessages, Product, Platform, VirtualHost);
             netqRpcConnections.Add(busKey, connection);
 
             return connection;
--- Blizzard.IO.RabbitMQ.Builders/Rpc/NetqRabbitRpcBuilder.cs
+++ Blizzard.IO.RabbitMQ.Builders/Rpc/NetqRabbitRpcBuilder.cs
@@ -1,7 +1,6 @@
 ﻿using Blizzard.IO.Core.Rpc;
 using Microsoft.Extensions.Logging;
 using Blizzard.IO.RabbitMQ.Rpc;
-using Blizzard.IO.Serialization.Rpc;
 using Blizzard.IO.RabbitMQ.Entities.Rpc;
 using System;
 
@@ -9,33 +8,13 @@ namespace Blizzard.IO.RabbitMQ.Builders.Rpc
 {
     public class NetqRabbitRpcBuilder : BaseNetqRabbitRpcBuilder
     {
-        private ISerializer _serializer = new JsonSerializer();
-        private string _hostname = "localhost";
-        private string _password = "guest";
-        private string _username = "guest";
-        private RpcConfiguration _rpcConfiguration = new RpcConfiguration
-        {
-            ExchangeNameProvider = type => "RPC_EXCHANGE",
-            RoutingKeyProvider = type => type.ToString()
-        };
-        private RpcMessageType _rpcMessageType = RpcMessageType.Concrete;
-        private ushort _timeout = 10;
-        private string _product = null;
-        private string _platform = null;
-        private string _virtualHost = null;
-        private int _requestHeartbeat = 10;
-        private int _prefetchCount = 50;
-        private bool _publisherConfirms = false;
-        private bool _persistentMessages = true;
-
-
         public NetqRabbitRpcBuilder(ILoggerFactory loggerFactory) : base(loggerFactory)
         {
         }
 
         public NetqRabbitRpcBuilder AddRpcMessageType(RpcMessageType rpcMessageType)
         {
-            _rpcMessageType = rpcMessageType;
+            RpcMessageType = rpcMessageType;
             return this;
         }
 
@@ -49,49 +28,47 @@ namespace Blizzard.IO.RabbitMQ.Builders.Rpc
             bool publisherConfirms = false,
             bool persistentMessages = true)
         {
-            _timeout = timeout;
-            _prefetchCount = prefetchCount;
-            _product = product;
-            _platform = platform;
-            _publisherConfirms = publisherConfirms;
-            _requestHeartbeat = requestHeartbeat;
-            _virtualHost = virtualHost;
-            _persistentMessages = persistentMessages;
+            Timeout = timeout;
+            PrefetchCount = prefetchCount;
+            Product = product;
+            Platform = platform;
+            PublisherConfirms = publisherConfirms;
+            RequestHeartbeat = requestHeartbeat;
+            VirtualHost = virtualHost;
+            PersistentMessages = persistentMessages;
             return this;
         }
 
         public NetqRabbitRpcBuilder AddCredentials(string hostname, string username, string password)
         {
-            _hostname = hostname;
-            _username = username;
-            _password = password;
+            Hostname = hostname;
+            Username = username;
+            Password = password;
             return this;
         }
 
         public NetqRabbitRpcBuilder AddSerializer(ISerializer serializer)
         {
-            _serializer = serializer;
+            Serializer = serializer;
             return this;
         }
 
         public NetqRabbitRpcBuilder AddRpcConfiguration(RpcConfiguration rpcConfiguration)
         {
-            _rpcConfiguration = rpcConfiguration;
+            RpcConfiguration = rpcConfiguration;
             return this;
         }
 
         public IRpcClient BuildClient()
         {
-            INetqRabbitRpcConnection<Func<Type, object>> connection = InitConnection(_hostname, _password, _username, _rpcConfiguration, _serializer,
-                _rpcMessageType, _timeout, _product, _platform, _virtualHost, _requestHeartbeat, _prefetchCount, _publisherConfirms, _persistentMessages);
+            INetqRabbitRpcConnection<Func<Type, object>> connection = InitConnection();
 
             return new NetqRabbitRpcClient(connection, LoggerFactory);
         }
 
         public IRpcServer BuildServer()
         {
-            INetqRabbitRpcConnection<Func<Type, object>> connection = InitConnection(_hostname, _password, _username, _rpcConfiguration, _serializer,
-                _rpcMessageType, _timeout, _product, _platform, _virtualHost, _requestHeartbeat, _prefetchCount, _publisherConfirms, _persistentMessages);
+            INetqRabbitRpcConnection<Func<Type, object>> connection = InitConnection();
 
             return new NetqRabbitRpcServer(connection, LoggerFactory);
         }
