d866c116f38197acf5cd84ebfa2de098d5bad344
alloncm <alloncm@gmail.com>
2020-04-06 14:53:35 +0300

    in the middle

---
+++ Blizzard.IO.RabbitMQ.Builders/BaseNetqRabbitBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using EasyNetQ;
+
+namespace Blizzard.IO.RabbitMQ.Builders
+{
+    public abstract class BaseNetqRabbitBuilder
+    {
+        protected static Dictionary<string, IBus> NetqRabbitConnections { get; private set; }
+
+        protected BaseNetqRabbitBuilder()
+        {
+            if (NetqRabbitConnections == null)
+            {
+                NetqRabbitConnections = new Dictionary<string, IBus>();
+            }
+        }
+
+        protected IBus InitConnection(string host, string password, string username)
+        {
+            string busKey = $"{host};{username};{password}";
+            if (NetqRabbitConnections.TryGetValue(busKey, out IBus bus))
+            {
+                return bus;
+            }
+
+            NetqRabbitConnections.Add(busKey, )
+        }
+
+        private IBus CreateBus(string host, string password, string username)
+        {
+            return RabbitHutch.CreateBus()
+        }
+    }
+}
