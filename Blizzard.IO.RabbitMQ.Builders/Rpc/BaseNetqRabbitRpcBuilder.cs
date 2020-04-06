4659097416ac91f2c3604b89062dbe608d4e1883
alloncm <alloncm@gmail.com>
2020-04-06 17:52:09 +0300

    added the base rpc builder

---
+++ Blizzard.IO.RabbitMQ.Builders/Rpc/BaseNetqRabbitRpcBuilder.cs
@@ -0,0 +1,50 @@
+﻿using Blizzard.IO.Core.Rpc;
+using System.Collections.Generic;
+using Blizzard.IO.RabbitMQ.Rpc;
+using System;
+using Blizzard.IO.RabbitMQ.Entities.Rpc;
+using Microsoft.Extensions.Logging;
+
+namespace Blizzard.IO.RabbitMQ.Builders.Rpc
+{
+    public class BaseNetqRabbitRpcBuilder
+    {
+        private static Dictionary<RpcConnectionKey, INetqRabbitRpcConnection<Func<Type, object>>> netqRpcConnections = new Dictionary<RpcConnectionKey, INetqRabbitRpcConnection<Func<Type, object>>>();
+
+        private readonly ILoggerFactory _loggerFactory;
+
+        public BaseNetqRabbitRpcBuilder(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        protected INetqRabbitRpcConnection<Func<Type, object>> InitConnection(
+            string host,
+            string password,
+            string username,
+            RpcConfiguration configuration,
+            ISerializer serializer,
+            RpcMessageType rpcMessageType = RpcMessageType.Concrete,
+            ushort timeout = 10,
+            string product = null,
+            string platform = null,
+            string virtualHost = null,
+            int requestHeartbeat = 10,
+            int prefetchCount = 50,
+            bool publisherConfirms = false,
+            bool persistentMessages = true)
+        {
+            RpcConnectionKey busKey = new RpcConnectionKey(host, username, password, rpcMessageType);
+            if (netqRpcConnections.ContainsKey(busKey))
+            {
+                return netqRpcConnections[busKey];
+            }
+
+            INetqRabbitRpcConnection<Func<Type,object>> connection = new NetqRabbitRpcConnection(configuration, host, username, password,_loggerFactory, serializer, requestHeartbeat, (ushort)prefetchCount, timeout,
+                publisherConfirms, persistentMessages, product, platform, virtualHost);
+            netqRpcConnections.Add(busKey, connection);
+
+            return connection;
+        }
+    }
+}
---
+++ Blizzard.IO.RabbitMQ.Builders/Rpc/RpcConnectionKey.cs
@@ -0,0 +1,20 @@
+﻿using Blizzard.IO.RabbitMQ.Entities.Rpc;
+
+namespace Blizzard.IO.RabbitMQ.Builders.Rpc
+{
+    public struct RpcConnectionKey
+    {
+        public string Hostname { get; }
+        public string Username { get; }
+        public string Password { get; }
+        public RpcMessageType RpcMessageType { get; }
+
+        public RpcConnectionKey(string hostname, string username, string password, RpcMessageType rpcMessageType)
+        {
+            Hostname = hostname;
+            Username = username;
+            Password = password;
+            RpcMessageType = rpcMessageType;
+        }
+    }
+}
