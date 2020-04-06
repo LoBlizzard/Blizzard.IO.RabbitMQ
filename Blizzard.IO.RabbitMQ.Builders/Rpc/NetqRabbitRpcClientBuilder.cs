7bd5a839d8ea42ce77bf9f38f0b2a8465b3a7bc2
alloncm <alloncm@gmail.com>
2020-04-06 18:49:30 +0300

    making it

--- Blizzard.IO.RabbitMQ.Builders/Rpc/NetqRabbitRpcClientBuilder.cs
+++ Blizzard.IO.RabbitMQ.Builders/Rpc/NetqRabbitRpcClientBuilder.cs
@@ -1,20 +1,32 @@
 ﻿using Blizzard.IO.Core.Rpc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Blizzard.IO.RabbitMQ.Rpc;
+using Blizzard.IO.Serialization.Rpc;
+using Blizzard.IO.RabbitMQ.Entities.Rpc;
 
 namespace Blizzard.IO.RabbitMQ.Builders.Rpc
 {
     public class NetqRabbitRpcClientBuilder : BaseNetqRabbitRpcBuilder
     {
+        private ISerializer _serializer = new JsonSerializer();
+        private string _hostname = "localhost";
+        private string _password = "guest";
+        private string _username = "guest";
+        private RpcConfiguration _rpcConfiguration = new RpcConfiguration 
+        {
+            ExchangeNameProvider = type=>"RPC_EXCHANGE",
+            RoutingKeyProvider = type=>type.ToString()
+        };
+
         public NetqRabbitRpcClientBuilder(ILoggerFactory loggerFactory) : base(loggerFactory)
         {
         }
 
         public IRpcClient Build()
         {
+            var connection = InitConnection(_hostname, _password, _username, _rpcConfiguration, _serializer);
 
+            return new NetqRabbitRpcClient()
         }
     }
 }
