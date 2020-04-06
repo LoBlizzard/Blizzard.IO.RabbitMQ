2464a26daf354722949a8b1fab87eba475ac037f
alloncm <alloncm@gmail.com>
2020-04-06 17:57:05 +0300

    added the builder

---
+++ Blizzard.IO.RabbitMQ.Builders/Rpc/NetqRabbitRpcClientBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Blizzard.IO.Core.Rpc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blizzard.IO.RabbitMQ.Builders.Rpc
+{
+    public class NetqRabbitRpcClientBuilder : BaseNetqRabbitRpcBuilder
+    {
+        public NetqRabbitRpcClientBuilder(ILoggerFactory loggerFactory) : base(loggerFactory)
+        {
+        }
+
+        public IRpcClient Build()
+        {
+
+        }
+    }
+}
